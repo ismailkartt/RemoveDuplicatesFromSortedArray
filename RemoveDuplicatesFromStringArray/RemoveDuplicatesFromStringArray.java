e1df4a87bc5e4f9eca8835c76de39e5fb79ca545
Ismail Kart <ismailkartt@gmail.com>
2023-06-23 11:42:38 +0300

    23/06/2023

---
+++ RemoveDuplicatesFromStringArray/RemoveDuplicatesFromStringArray.java
@@ -0,0 +1,42 @@
+package codingbat;
+
+import java.util.*;
+
+public class RemoveDuplicatesFromStringArray {
+
+
+    public static List<String> removeDuplicatesWithForLoop(List<String> arr){
+        List<String> result = new ArrayList<>();
+
+        for(int i=0; i<arr.size(); i++){
+            boolean flag = false;
+            for(int j=0; j<result.size(); j++){
+                if(result.get(j) == arr.get(i)){ //"Kadir", "Kemal", "Kemal", "Dursun", "Kadir", "Kemal"
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag){
+                result.add(arr.get(i));
+            }
+        }
+        return result;
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        List<String> input = Arrays.asList("Kadir", "Kemal", "Kemal", "Dursun", "Kadir", "Kemal");
+
+        List<String> output1 = removeDuplicatesWithForLoop(input);
+        System.out.println(output1);
+
+//        List<String> output2 = removeDuplicatesWithSet(input);
+//        System.out.println(output2);
+
+    }
+
+
+}
